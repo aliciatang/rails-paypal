a0106950aa7d9410427a906f9155db594a687075
Alicia Tang <alicia.x.tang@gmail.com>
2012-09-07 21:46:03 -0700

    add test for static method

--- test/test_express_checkout.rb
+++ test/test_express_checkout.rb
@@ -17,13 +17,17 @@ class ExpressCheckoutTest < Test::Unit::TestCase
 	end
 	def test_get_success
 		ret = RailsPaypal::ExpressCheckout.new([{:name=>'item 1',:price=>'5.0',:quantity=> 4}])
-        assert_nothing_raised do
-          ret = ret.get
-        end
-        assert_not_nil ret["CHECKOUTSTATUS"]
+    assert_nothing_raised do
+      ret = ret.get
+    end
+    assert_not_nil ret["CHECKOUTSTATUS"]
 	end
 	def test_get_redirect_url
 		ret = RailsPaypal::ExpressCheckout.new([{:name=>'item 1',:price=>'5.0',:quantity=> 4}])
-        assert_not_nil ret.redirect_url
+    assert_not_nil ret.redirect_url
 	end
+  def test_get_paid_success
+    ret = RailsPaypal::ExpressCheckout::get('EC-3EC18176EP034311U')
+    assert_equal "Success",  ret["ACK"]
+  end
 end
